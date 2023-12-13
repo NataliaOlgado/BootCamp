0100091bf094ad89cebd9bd88492f4be3a18ad7d
NataliaOlgado <natalia.olgado@hotmail.com>
2023-12-13 12:20:47 -0300

    Atividades de Fila e Pilha

---
+++ ExercicioST/LacosRepeticao/src/LacosRepeticao/Exerciciolacosderepetição.java
@@ -0,0 +1,12 @@
+package LacosRepeticao;
+
+import java.util.Scanner;
+
+public class Exerciciolacosderepetição {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ExercicioST/LacosRepeticao/src/LacosRepeticao/Treino1.java
@@ -0,0 +1,14 @@
+package LacosRepeticao;
+import java.util.Scanner;
+
+public class Treino1 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+
+		
+		
+
+	}
+
+}
